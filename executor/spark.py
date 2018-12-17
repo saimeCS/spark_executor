ca32abbf8adedc172f057309ec235d887cbe3833
Stéphan AIME <stephan.aime@c-s.fr>
2018-12-17 17:15:01 +0100

    All items from context are used dynamically

--- executor/spark.py
+++ executor/spark.py
@@ -18,19 +18,25 @@ class SparkExecutor(BaseExecutor):
             raise ValueError('Context is mandatory for SparkExecutor')
         self._context = context
 
+    def _get_spark_conf(self):
+        conf = SparkConf()
+
+        for key in self._context.keys():
+            if key in 'spark_app_name':
+                conf.setAppName(self._context['spark_app_name'])
+                continue
+            if key in 'spark_url_master':
+                conf.setMaster(self._context['spark_url_master'])
+                continue
+            conf.set(key, self._context.get(key))
+
+        return conf
+
     def execute(self, func, items):
         try:
             logger.info('In spark executor')
 
-            conf = SparkConf().setAppName(self._context['spark_app_name']) \
-                .setMaster(self._context['spark_url_master']) \
-                .set("spark.task.cpus", self._context.get('spark.task.cpus', 16)) \
-                .set("spark.default.parallelism",
-                     self._context.get('spark.default.parallelism', 16)) \
-                .set("spark.python.worker.memory",
-                     self._context.get('spark.python.worker.memory', '50g')) \
-                .set("spark.task.maxFailures", self._context.get('spark.task.maxFailures', 1))
-            sc = SparkContext(conf=conf)
+            sc = SparkContext(conf=self._get_spark_conf())
 
             rv = sc.parallelize(items).map(func).collect()
             sc.stop()
--- executor/tests/test_spark.py
+++ executor/tests/test_spark.py
@@ -26,3 +26,18 @@ class TestSpark(unittest.TestCase):
         ctx.with_kv('spark.python.worker.memory', 1)
         spark_executor = SparkExecutor(ctx)
         print(spark_executor.execute(echo, items))
+
+    def test_context_standard(self):
+        ctx = ExecutionContext()
+        ctx.with_kv('spark_app_name', 'my test app')
+        ctx.with_kv('spark_url_master', 'local[*]')
+        ctx.with_kv('spark.task.cpus', 2)
+        ctx.with_kv('spark.default.parallelism', 4)
+        ctx.with_kv('spark.python.worker.memory', 1)
+        spark_executor = SparkExecutor(ctx)
+        conf = spark_executor._get_spark_conf()
+        self.assertEqual('my test app', conf.get('spark.app.name'))
+        self.assertEqual('local[*]', conf.get('spark.master'))
+        self.assertEqual('2', conf.get('spark.task.cpus'))
+        self.assertEqual('1', conf.get('spark.python.worker.memory'))
+        self.assertIsNone(conf.get('unset_property'))
